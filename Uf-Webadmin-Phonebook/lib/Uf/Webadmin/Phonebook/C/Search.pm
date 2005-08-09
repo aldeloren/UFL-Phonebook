0d5663198a38ea16b3356ca68d9277186fc7b5f5
Daniel Westermann-Clark <dwc@ufl.edu>
2005-08-09 15:19:03 +0000

    Add search controller component

---
+++ Uf-Webadmin-Phonebook/lib/Uf/Webadmin/Phonebook/C/Search.pm
@@ -0,0 +1,42 @@
+package Uf::Webadmin::Phonebook::C::Search;
+
+use strict;
+use base 'Catalyst::Base';
+
+=head1 NAME
+
+Uf::Webadmin::Phonebook::C::Search - Search controller component
+
+=head1 SYNOPSIS
+
+See L<Uf::Webadmin::Phonebook>
+
+=head1 DESCRIPTION
+
+Handle search requests.
+
+=head1 METHODS
+
+=head2 default
+
+=cut
+
+sub default : Private {
+    my ( $self, $c ) = @_;
+    $c->res->output('Congratulations, Uf::Webadmin::Phonebook::C::Search is on Catalyst!');
+}
+
+=back
+
+=head1 AUTHOR
+
+University of Florida Web Administration E<lt>webmaster@ufl.eduE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
