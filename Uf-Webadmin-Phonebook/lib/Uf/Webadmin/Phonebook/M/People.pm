38da4057d2db050f4f7c7bed227f3c93f72cff0d
Daniel Westermann-Clark <dwc@ufl.edu>
2005-08-11 17:23:33 +0000

    Override search method in the People model component and add basic code for parsing a query into an LDAP filter.  It would be nice to have a more abstract iterface for creating LDAP filters, perhaps like SQL::Abstract.

--- Uf-Webadmin-Phonebook/lib/Uf/Webadmin/Phonebook/M/People.pm
+++ Uf-Webadmin-Phonebook/lib/Uf/Webadmin/Phonebook/M/People.pm
@@ -24,6 +24,46 @@ See L<Uf::Webadmin::Phonebook>
 Catalyst model component for the University of Florida LDAP server.
 This component uses a base of C<ou=People,dc=ufl,dc=edu>.
 
+=head1 METHODS
+
+=head2 search
+
+Search the directory for a person.
+
+=cut
+
+sub search {
+    my ($self, $query) = @_;
+
+    if ($query =~ /[^a-z0-9 ._\'*\-\@]/) {
+        die 'Query contains invalid characters';
+    }
+
+    my $filter = $self->_parseQuery($query);
+    $self->SUPER::search($filter);
+}
+
+=head2 _parseQuery
+
+=cut
+
+sub _parseQuery {
+    my ($self, $query) = @_;
+
+    my @tokens = split(/\s+/, lc($query));
+
+    my $filter;
+    if (my $pos = $query =~ m/\@/) {
+        my $email = shift @tokens;
+        $filter = 'email=' . $email;
+    }
+    else {
+        $filter = 'sn=' . $query;
+    }
+
+    return $filter;
+}
+
 =head1 AUTHOR
 
 University of Florida Web Administration E<lt>webmaster@ufl.eduE<gt>
