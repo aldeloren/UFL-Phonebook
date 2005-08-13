c35551c3d96e80ace125c59f122b532712f2caa8
Daniel Westermann-Clark <dwc@ufl.edu>
2005-08-13 00:23:49 +0000

    Autocomplete even when text is in front of the cursor, i.e. not just at the end.  This illustrates the need for canceling the autocompletion e.g. when delete is pressed.

--- Uf-Webadmin-Phonebook/root/static/js/autocomplete.js
+++ Uf-Webadmin-Phonebook/root/static/js/autocomplete.js
@@ -1,16 +1,28 @@
 function autocomplete(f, completions) {
 	var field = $(f);
 
-	for (completion in completions) {
-		if (typeof completion == "RegExp") {
-			if (completion.test(field.value)) {
-				alert("regexp match!");
+	var completion = null;
+	for (c in completions) {
+		if (typeof c == "RegExp") {
+			if (c.test(field.value)) {
+				completion = completion[c];
+				break;
 			}
 		}
 		else {
-			if (field.value.substr(field.value.length - completion.length, field.value.length) == completion) {
-				field.value += completions[completion];
+			if (field.value.substr(field.selectionEnd - c.length, c.length) == c) {
+				completion = completions[c];
+				break;
 			}
 		}
+	}
+
+	if (completion) {
+		var before = field.value.substr(0, field.selectionStart);
+		var after  = field.value.substr(field.selectionEnd, field.value.length);
+
+		var pos = field.selectionEnd;
+		field.value = before + completion + after;
+		field.setSelectionRange(pos, pos + completion.length);
 	}
 }
