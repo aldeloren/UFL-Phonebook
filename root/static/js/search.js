d7091f516f3029988797abecfbbbec935a65f250
Daniel Westermann-Clark <dwc@ufl.edu>
2006-02-16 06:04:07 +0000

    More JavaScript fixups

--- root/static/js/search.js
+++ root/static/js/search.js
@@ -1,31 +1,52 @@
+/*
+ * Clear the default value of any input fields with class "query" when
+ * clicked.
+ */
 var Query = {
 	defaultValues: {},
 
+	setDefaultValue: function(key, value) {
+		// Let page override value from init
+		if (Query.defaultValues[key] == null) {
+			Query.defaultValues[key] = value;
+		}
+	},
+	getDefaultValue: function(key) {
+		return Query.defaultValues[key];
+	},
 	init: function() {
 		var inputs = document.getElementsByTagName('input');
 		for (var i = 0; i < inputs.length; i++) {
 			var input = inputs[i];
-			if (Element.hasClassName(input, "query")) {
-				Query.defaultValues[input.id] = input.value;
+			if (Element.hasClassName(input, 'query')) {
+				Query.setDefaultValue(input.id, input.value);
 				addEvent(input, 'onclick', Query.click);
 				addEvent(input, 'onblur', Query.blur);
+
+				if (input.value != Query.getDefaultValue(input.id)) {
+					Element.addClassName(input, 'active');
+				}
 			}
 		}
 	},
 	click: function() {
-		Element.addClassName(this, "active");
-		if (this.value == Query.defaultValues[this.id]) {
+		Element.addClassName(this, 'active');
+		if (this.value == Query.getDefaultValue(this.id)) {
 			this.value = '';
 		}
 	},
 	blur: function() {
 		if (this.value == '') {
 			Element.removeClassName(this, 'active');
-			this.value = Query.defaultValues[this.id];
+			this.value = Query.getDefaultValue(this.id);
 		}
 	}
 };
 
+/*
+ * Automatically select the first input field with class "query" in
+ * the form containing a "source" select menu.
+ */
 var Source = {
 	init: function() {
 		var source = $('source');
@@ -34,8 +55,14 @@ var Source = {
 		}
 	},
 	change: function() {
-		var query = $('query');
-		Field.activate(query);
+		var inputs = this.form.getElementsByTagName('input');
+		for (var i = 0; i < inputs.length; i++) {
+			var input = inputs[i];
+			if (Element.hasClassName(input, 'query')) {
+				Field.activate(input);
+				break;
+			}
+		}
 	}
 };
 
