7d19d82bdff13cc33c976d798e5306cf67cfcc35
Daniel Westermann-Clark <dwc@ufl.edu>
2007-07-26 22:35:08 +0000

    Move actions from application to Controller::Root and switch to RenderView

--- lib/UFL/Phonebook.pm
+++ lib/UFL/Phonebook.pm
@@ -34,64 +34,6 @@ It is written using the L<Catalyst> framework.
 
 =head1 METHODS
 
-=head2 index
-
-Display the home page.
-
-=cut
-
-sub index : Private {
-    my ($self, $c) = @_;
-
-    $c->stash->{template} = 'index.tt';
-}
-
-=head2 default
-
-Handle any actions which did not match, i.e. 404 errors.
-
-=cut
-
-sub default : Private {
-    my ($self, $c, $path) = @_;
-
-    $c->res->status(404);
-    $c->stash->{template} = '404.tt';
-}
-
-=head2 end
-
-Forward to the view.
-
-=cut
-
-sub end : Private {
-    my ($self, $c) = @_;
-
-    return 1 if $c->res->status =~ /^3\d\d$/;
-    return 1 if $c->res->body;
-
-    if (@{ $c->error }) {
-        $c->res->status(500);
-
-        # Override the ugly Catalyst debug screen
-        unless ($c->debug) {
-            $c->log->error($_) for @{ $c->error };
-
-            $c->stash->{errors} = $c->error;
-            $c->error(0);
-
-            $c->stash->{template} = 'error.tt';
-        }
-    }
-
-    $c->forward($c->view('TT'));
-}
-
-##
-## Application methods
-##
-
 =head2 uri_for
 
 Overload C<uri_for> to handle query parameters and to accept objects
---
+++ lib/UFL/Phonebook/Controller/Root.pm
@@ -0,0 +1,92 @@
+package UFL::Phonebook::Controller::Root;
+
+use strict;
+use warnings;
+use base qw/Catalyst::Controller/;
+
+__PACKAGE__->config->{namespace} = '';
+
+=head1 NAME
+
+UFL::Phonebook::Controller::Root - Root controller
+
+=head1 DESCRIPTION
+
+Root L<Catalyst> controller for L<UFL::Phonebook>.
+
+=head1 METHODS
+
+=head2 default
+
+Handle any actions which did not match, i.e. 404 errors.
+
+=cut
+
+sub default : Private {
+    my ($self, $c) = @_;
+
+    $c->res->status(404);
+    $c->stash(template => 'not_found.tt');
+}
+
+=head2 index
+
+Display the home page.
+
+=cut
+
+sub index : Path('') Args(0) {
+    my ($self, $c) = @_;
+
+    $c->stash(template  => 'index.tt');
+}
+
+=head2 render
+
+Attempt to render a view, if needed.
+
+=cut 
+
+sub render : ActionClass('RenderView') {
+    my ($self, $c) = @_;
+
+    if (@{ $c->error }) {
+        $c->res->status(500);
+
+        # Override the ugly Catalyst debug screen
+        unless ($c->debug) {
+            $c->log->error($_) for @{ $c->error };
+
+            $c->stash(
+                errors   => $c->error,
+                template => 'error.tt',
+            );
+            $c->clear_errors;
+        }
+    }
+}
+
+=head2 end
+
+Render a view and finish up before sending the response.
+
+=cut
+
+sub end : Private {
+    my ($self, $c) = @_;
+
+    $c->forward('render');
+}
+
+=head1 AUTHOR
+
+Daniel Westermann-Clark E<lt>dwc@ufl.eduE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
