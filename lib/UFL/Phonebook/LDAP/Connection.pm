1fccaf7af2a462c35cbc4f5ad90bb3d354667296
Daniel Westermann-Clark <dwc@ufl.edu>
2007-08-17 23:29:59 +0000

    First pass at doing kinit as part of the connection

--- lib/UFL/Phonebook/LDAP/Connection.pm
+++ lib/UFL/Phonebook/LDAP/Connection.pm
@@ -6,6 +6,7 @@ use base qw/Catalyst::Model::LDAP::Connection/;
 use Authen::SASL qw/Perl/;
 use Carp qw/croak/;
 use Class::C3;
+use IPC::Open3 qw//;
 use Net::LDAP::Control::ProxyAuth;
 
 __PACKAGE__->mk_accessors(qw/catalyst_user/);
@@ -25,22 +26,76 @@ of the person associated with the current L<Catalyst> user.
 
 Bind the connection, authenticating via SASL.
 
+    $conn->bind(
+        host => 'ldap.ufl.edu',
+        krb5 => {
+            principal => 'user@ufl.edu',
+            keytab    => '/home/user/keytab',
+            lifetime  => 86400,  # 1 day
+        },
+        sasl => {
+            service => 'user@ufl.edu',
+        },
+    );
+
 =cut
 
 sub bind {
     my ($self, %args) = @_;
 
+    my %krb5_args = %{ delete $args{krb5} || {} };
     my %sasl_args = %{ delete $args{sasl} || {} };
 
     if (my $catalyst_user = delete $args{catalyst_user}) {
         # Store the Catalyst user for later
         $self->catalyst_user($catalyst_user);
 
+        $self->_krb5_login(%krb5_args);
+
         my $sasl = Authen::SASL->new(mechanism => 'GSSAPI', %sasl_args);
         $args{sasl} = $sasl;
     }
 
     $self->next::method(%args);
+}
+
+=head2 _krb5_login
+
+Request a ticket using C<kinit> and the specific arguments.
+
+    $self->_krb5_login(
+        principal => 'user@ufl.edu',
+        keytab    => '/home/user/keytab',
+        lifetime  => 86400,  # 1 day
+        command   => '/usr/local/bin/kinit',
+        timeout   => 10,  # Wait 10 seconds for kinit to finish
+    );
+
+=cut
+
+sub _krb5_login {
+    my ($self, %args) = @_;
+
+    my $principal = $args{principal};
+    my $keytab    = $args{keytab};
+    my $lifetime  = $args{lifetime} || 3600;
+    my $command   = $args{command} || '/usr/bin/kinit';
+    my $timeout   = $args{timeout} || 10;
+
+    die 'No principal found' unless $principal;
+    die 'No keytab found' unless $keytab and -f $keytab;
+    die 'No kinit command available' unless -x $command;
+
+    my @cmd = ($command, '-l', $lifetime, '-k', '-t', $keytab, $principal);
+    my ($in, $out, $err);
+    eval {
+        local $SIG{ALRM} = sub { die "time out" };
+        alarm $timeout;
+        my $pid = IPC::Open3::open3($in, $out, $err, @cmd);
+        waitpid $pid, 0;
+        alarm 0;
+    };
+    die "kinit failed: $@" if $@;
 }
 
 =head2 search
