59217b4638e31a9af8ca7054ec5877e3cc7ec2c8
Daniel Westermann-Clark <dwc@ufl.edu>
2010-05-17 21:45:37 +0000

    Spacing

--- lib/UFL/Phonebook/Controller/Throttle.pm
+++ lib/UFL/Phonebook/Controller/Throttle.pm
@@ -114,8 +114,10 @@ sub check : Private {
     my $ip = $c->req->address;
     unless ($self->_throttler->try_push(key => $ip)) {
         $c->log->info("Throttling request from [$ip]");
+
         $self->_throttled_ips->{$ip} = DateTime->now(time_zone => 'local')
             unless exists $self->_throttled_ips->{$ip};
+
         $c->detach('/unavailable');
     }
 }
